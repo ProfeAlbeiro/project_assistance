c81ab4a9c5c15b3b091b905c12f39a268f6d20a0
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-06-11 15:50:15 -0500

    05 - Usuarios

--- views/modules/users/rol_read.view.php
+++ views/modules/users/rol_read.view.php
@@ -16,7 +16,7 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Consultar Roles</h5>
-
+              <!-- Hola -->
               <!-- Table with stripped rows -->
               <table class="table datatable">
                 <thead>
